c15c45b3fe688ca80b61f0ccbc27927e318c7e9f
Andres98100 <andressolarte580@gmail.com>
2023-04-06 14:56:44 -0500

    debug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,10 +16,10 @@ def display_states():
 
 
 @app.teardown_appcontext
-def close_db():
+def close_db(error):
     """close"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)

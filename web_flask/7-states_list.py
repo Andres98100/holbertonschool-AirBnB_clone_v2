5fe6640ce1d28bfa2ca27579d51a2d332cb3873d
Andres98100 <andressolarte580@gmail.com>
2023-04-05 19:22:48 -0500

    state_list with flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""import"""
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+
+"""decorate"""
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """return"""
+    state = storage.all(State).values()
+    return render_template("7-states_list.html", state=state)
+
+
+"""decorate"""
+@app.teardown_appcontext
+def close_db():
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

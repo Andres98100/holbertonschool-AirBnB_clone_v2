06d0a13779e7a3e98d2a1c34be311ae61b9ba8db
Andres98100 <andressolarte580@gmail.com>
2023-04-06 15:07:36 -0500

    debug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def display_states():
     """variable show the list"""
-    state = storage.all(State).values()
+    state = sorted(storage.all(State).values())
     return render_template("7-states_list.html", states=state)
 
 

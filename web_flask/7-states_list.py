5094cb9808e52d25a4d5d8703e03968de4cea08a
Andres98100 <andressolarte580@gmail.com>
2023-04-06 14:49:59 -0500

    import state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""import"""
+"""import flask and models"""
 from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -9,15 +10,14 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def display_states():
-    """variable"""
+    """variable show the list"""
     state = storage.all(State).values()
-    """return"""
     return render_template("7-states_list.html", state=state)
 
 
 @app.teardown_appcontext
 def close_db():
-    """storage"""
+    """close"""
     storage.close()
 
 
